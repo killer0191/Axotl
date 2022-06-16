3978ac96ed28491b9313df60cbd6f2fc41a142c1
killer0191 <99231496+killer0191@users.noreply.github.com>
2022-06-15 21:31:28 -0500

    Update otros.js

--- js/otros.js
+++ js/otros.js
@@ -4,16 +4,16 @@ const datos_car=[
     title:"Taza de caf\xe9 de ajolote",
     fecha:" Fecha Salidad:01/3/2021",
 	description:"Taza de café de alta calidad con estampado de ajolote, color blanco.",
-    price_before:"S/275.50",
-    price_now:"S/192.85",
+    price_before:"$275.50",
+    price_now:"$192.85",
   },
    {
     img:"img/img5.jpg",
     title:"Libreta de Ajolote",
     fecha:" Fecha Salidad:01/3/2021",
 	description:"Libreta/notebook profesional rayada de ajolote color rosa con 80 hojas.",
-    price_before:"S/125.50",
-    price_now:"S/87.85",
+    price_before:"$125.50",
+    price_now:"$87.85",
   },
 ]
 
